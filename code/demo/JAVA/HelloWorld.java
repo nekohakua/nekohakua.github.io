56d06628be93e2e4ceee69f96a56c03454c24da7
lambta.pan <lambta.panweiqiang@fenda.com>
2022-05-25 13:53:01 +0800

    web

---
+++ code/demo/JAVA/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    /* 第一个Java程序
+     * 它将输出字符串 Hello World
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World00"); 
+    }
+}
